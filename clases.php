1a38a96ba3d072eb25a7e1fc55c150e35d89b1dd
Esteban Sinobas Carpio <esteban.sinobas@gmail.com>
2013-10-30 11:23:23 +0100

    Slipt contenidos.php

---
+++ clases.php
@@ -0,0 +1,93 @@
+<div id="cuerpocenter_clases_borde_top"></div>
+<div id="cuerpocenter_clases">
+	
+	<object id="FlashID2" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="585" height="474">
+	  <param name="movie" value="img/colectiva.swf" />
+	  <param name="quality" value="high" />
+	  <param name="wmode" value="transparent" />
+	  <param name="swfversion" value="6.0.65.0" />
+	  <!-- Esta etiqueta param indica a los usuarios de Flash Player 6.0 r65 o posterior que descarguen la versión más reciente de Flash Player. Elimínela si no desea que los usuarios vean el mensaje. -->
+	  <param name="expressinstall" value="Scripts/expressInstall.swf" />
+	  <!-- La siguiente etiqueta object es para navegadores distintos de IE. Ocúltela a IE mediante IECC. -->
+	  <!--[if !IE]>-->
+	  <object type="application/x-shockwave-flash" data="img/colectiva.swf" width="585" height="474">
+	    <!--<![endif]-->
+	    <param name="quality" value="high" />
+	    <param name="wmode" value="transparent" />
+	    <param name="swfversion" value="6.0.65.0" />
+	    <param name="expressinstall" value="Scripts/expressInstall.swf" />
+	    <!-- El navegador muestra el siguiente contenido alternativo para usuarios con Flash Player 6.0 o versiones anteriores. -->
+	    <div>
+	      <h4>El contenido de esta p&aacute;gina requiere una versi&oacute;n m&aacute;s reciente de Adobe Flash Player.</h4>
+	      <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Obtener Adobe Flash Player" width="112" height="33" /></a></p>
+        </div>
+	    <!--[if !IE]>-->
+      </object>
+	  <!--<![endif]-->
+  </object>
+<div id="texto_clases">
+    <span class="titulo">CLASES COLECTIVAS</span>
+    <div style="width:340px">
+    	<p>
+			<b>Aerobic-Step:</b> ¡No pierdas el paso! Y únete a la clase más divertida de Leganés. Los pasos más dinámicos para moldear tu cuerpo y quemar calorías te están esperando.
+        </p>
+        <p>
+			<b>GAP:</b> (Glúteos, abdominales y piernas) nuestros asesores trabajarán específicamente estas zonas de tu cuerpo, combinando ejercicios en el suelo con acciones dinámicas. ¡Verás los resultados! 
+        </p>
+        <p>
+        	<b>Ciclo Indoor:</b> ¡Pedalea al ritmo de la música! Desde principiantes hasta expertos disfrutan quemando calorías y mejorando su sistema cardiovascular.
+        </p>
+    	<p>
+       		<b>Yoga:</b> La disciplina justa para liberar, oxigenar y mantener tu cuerpo, mente y espíritu en total armonía. Aquí en LegaFitness y con los mejores asesores de meditación podrás cultivar tu equilibrio, con esta gran clase colectiva.
+        </p>
+        <p>
+			<b>Pilates:</b> Fortalecimiento muscular y estiramientos dirigidos a ejercitar tu cuerpo y mente; mejorar tu equilibrio y flexibilidad, y por supuesto, tu salud.
+        </p>
+        <p>
+        	<b>Tai-Chi</b> La tradición oriental para mejorar y restablecer tu salud. Deja llevarte por la filosofía tradicional china controlando la respiración y equilibrando tu cuerpo.
+        </p>
+     </div>
+     <br />
+        <div style="background-color:#DFDFDF;padding:10px">
+        <b>Reglamento de uso:</b>
+        
+        <ol>
+        <li> Es obligatorio el acceso a la sala con calzado deportivo, ropa deportiva y toalla.</li>
+        
+        <li> No se permite el acceso a clases sin camiseta, en bañador o con zapatillas de baño.</li>
+        
+        <!-- 
+        <li> La asistencia a las clases se hará por riguros orden de inscripción. Las clases colectivas se darán con un mínimo de tres personas</li>
+        
+        <li> 30 minutos antes del comienzo de cada clase estará disponible en recepción una hoja de inscripción con un número limitado de plazas.</li>
+        -->
+        
+        <li> Una vez completado el aforo de plazas no se podrá asistir a la clase.</li>
+        
+       <li> Se ruega máxima puntualidad en el inicio de la clase, ya que el calentamiento es esencial para un buen entrenamiento, al igual que es muy importante no abandonar la sesión antes de que ésta finalice.</li>
+        
+        <li> Si sufre algún tipo de lesión, enfermedad o toma medicación especial que pueda afectarle en la práctica del ejercicio físico, le rogamos se lo comunique al monitor responsable de la clase.
+
+</li>
+        
+        <li> Asegúrese de que el contenido e intensidad de las sesiones son los adecuados a su nivel actual de condición física.</li>
+        
+        <li> Por razones de higiene es obligatorio usar toalla en los ejercicios de suelo.</li>
+        
+        <li> Procure hidratarse antes y después de la práctica del ejercicio físico. Es recomendable beber 200 ml. de agua cada 15 ó 20 minutos.</li>
+        
+        <li> No se permite el uso del teléfono móvil dentro de la sala.</li>
+        
+        <li> Para poder acceder a una clase, deberá esperar que finalice la anterior. </li>
+        
+        
+        <li> No se permite fumar, comer e introducir objetos de vidrio en la sala.</li>
+        
+        
+        
+        </ol>
+        </div>
+    </div>
+</div>
+<div id="cuerpocenter_clases_borde_bottom"></div>
+<div style="text-align:center"><img src="img/tu_fitness.jpg" /></div>
---
+++ contacto.php
@@ -0,0 +1,46 @@
+<div id="cuerpocenter_contacto">
+  <object id="FlashID" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="585" height="474">
+	  <param name="movie" value="img/contacto.swf" />
+	  <param name="quality" value="high" />
+	  <param name="wmode" value="transparent" />
+	  <param name="swfversion" value="6.0.65.0" />
+	  <!-- Esta etiqueta param indica a los usuarios de Flash Player 6.0 r65 o posterior que descarguen la versión más reciente de Flash Player. Elimínela si no desea que los usuarios vean el mensaje. -->
+	  <param name="expressinstall" value="Scripts/expressInstall.swf" />
+	  <!-- La siguiente etiqueta object es para navegadores distintos de IE. Ocúltela a IE mediante IECC. -->
+	  <!--[if !IE]>-->
+	  <object type="application/x-shockwave-flash" data="img/contacto.swf" width="585" height="474">
+	    <!--<![endif]-->
+	    <param name="quality" value="high" />
+	    <param name="wmode" value="transparent" />
+	    <param name="swfversion" value="6.0.65.0" />
+	    <param name="expressinstall" value="Scripts/expressInstall.swf" />
+	    <!-- El navegador muestra el siguiente contenido alternativo para usuarios con Flash Player 6.0 o versiones anteriores. -->
+	    <div>
+	      <h4>El contenido de esta p&aacute;gina requiere una versi&oacute;n m&aacute;s reciente de Adobe Flash Player.</h4>
+	      <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Obtener Adobe Flash Player" width="112" height="33" /></a></p>
+        </div>
+	    <!--[if !IE]>-->
+    </object>
+	  <!--<![endif]-->
+  </object>
+<div id="texto_contacto">
+    	<span class="titulo">CONTACTO</span>
+        <p>
+      	 E-mail: <a href="mailto:info@legafitness.es" class="mailto">info@legafitness.es</a>.<br />
+        </p>
+        <p>
+			Dirección: C/ El Charco, Nº 13, 28911		<br />Leganés, Madrid
+        </p>
+        <p>
+        	<table>
+            	<tr><td width="97" align="center"><img src="img/cercanias.jpg" /></td><td>Leganés Central.</td></tr>
+                <tr><td align="center"><img src="img/metro.jpg" /></td><td>Leganés Central.</td></tr>
+               <tr><td align="center"><img src="img/bus.gif" /></td><td>432-450-480-482<br />484-485-486</td></tr>
+              </table>
+        </p>
+        
+    </div>
+    <div id="mapa">
+    <iframe width="280" height="442" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.google.es/maps?f=q&amp;source=s_q&amp;hl=es&amp;geocode=&amp;q=C%2F+El+Charco,+N%C2%BA+13,+28911+&amp;aq=&amp;sll=40.396764,-3.713379&amp;sspn=13.445309,33.771973&amp;ie=UTF8&amp;hq=&amp;hnear=Calle+del+Charco,+13,+28911+Legan%C3%A9s,+Comunidad+de+Madrid&amp;ll=40.327309,-3.768311&amp;spn=0.028922,0.023947&amp;z=14&amp;output=embed"></iframe><br />
+    </div>
+</div>	
