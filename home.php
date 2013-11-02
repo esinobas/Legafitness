9a912621c470d11256163eef917df568506dcf87
Esteban Sinobas Carpio <tebi@Ubuntu-Webserver.(none)>
2013-11-03 00:58:08 +0100

    First Version of ImageSlider

--- home.php
+++ home.php
@@ -71,31 +71,66 @@ class="boton"><?php if(@$_GET['p'] == 'clases'){ ?> <span class="bizq"></span><a
 					else
 					{
 					?>
+					      <script src="./plugins/ImageSlider/ImageSlider.js" type="text/javascript" ></script>
                     	<div id="cuerpocenter_home">
-                          <object id="FlashID2" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="585" height="474">
-                            <param name="movie" value="img/cuerpo.swf" />
-                            <param name="quality" value="high" />
-                            <param name="wmode" value="transparent" />
-                            <param name="swfversion" value="6.0.65.0" />
-                            <!-- Esta etiqueta param indica a los usuarios de Flash Player 6.0 r65 o posterior que descarguen la versión más reciente de Flash Player. Elimínela si no desea que los usuarios vean el mensaje. -->
-                            <param name="expressinstall" value="Scripts/expressInstall.swf" />
-                            <!-- La siguiente etiqueta object es para navegadores distintos de IE. Ocúltela a IE mediante IECC. -->
-                            <!--[if !IE]>-->
-                            <object type="application/x-shockwave-flash" data="img/cuerpo.swf" width="585" height="474">
-                              <!--<![endif]-->
-                              <param name="quality" value="high" />
-                              <param name="wmode" value="transparent" />
-                              <param name="swfversion" value="6.0.65.0" />
-                              <param name="expressinstall" value="Scripts/expressInstall.swf" />
-                              <!-- El navegador muestra el siguiente contenido alternativo para usuarios con Flash Player 6.0 o versiones anteriores. -->
-                              <div>
-                                <h4>El contenido de esta página requiere una versión más reciente de Adobe Flash Player.</h4>
-                                <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Obtener Adobe Flash Player" width="112" height="33" /></a></p>
-                              </div>
-                              <!--[if !IE]>-->
-                            </object>
-                            <!--<![endif]-->
-                          </object>
+                    	   <?php
+                    	      include_once('./php/DirectoryFunctions.php'); 
+                    	   ?>
+                        <div id="cuerpocenter_cuerpo1">
+                           <?php
+                              $files = getDirectoryFiles('./img/cuerpo/cuerpo1');
+                              for ($i = 0; $i < count($files);$i++){
+                                 printf("<img src=\"%s\" alt=\"\">\n", $files[$i]);
+                              }
+                           ?>
+                        </div>
+                        
+
+                       <script type="text/javascript" >
+                          ImageSlider.init(
+                          {
+                             container: "cuerpocenter_cuerpo1",
+                             timeout: 3
+                          }   
+                          );
+                      </script>
+                        
+                        <div id="cuerpocenter_cuerpo2">
+                           <?php
+                              $files = getDirectoryFiles('./img/cuerpo/cuerpo2');
+                              for ($i = 0; $i < count($files);$i++){
+                                 printf("<img src=\"%s\" alt=\"\">\n", $files[$i]);
+                              }
+                           ?>                        
+                        </div>
+                        
+                        <script type="text/javascript" >
+                          ImageSlider.init(
+                          {
+                             container: "cuerpocenter_cuerpo2",
+                             timeout: 3.5
+                          }   
+                          );
+                      </script>
+                        
+                      
+                        <div id="cuerpocenter_cuerpo3">
+                           <?php
+                              $files = getDirectoryFiles('./img/cuerpo/cuerpo3');
+                              for ($i = 0; $i < count($files);$i++){
+                                 printf("<img src=\"%s\" alt=\"\">\n", $files[$i]);
+                              }
+                           ?>
+                        </div>
+                        <script type="text/javascript" >
+                          ImageSlider.init(
+                          {
+                             container: "cuerpocenter_cuerpo3",
+                             timeout: 4
+                          }   
+                          );
+                      </script>                        
+                        
                     	</div>
                     <?php	
 					}
@@ -159,15 +194,6 @@ Leganés, Madrid
 <div>
 Me cago en to
 </div>
-<script src="./plugins/ImageSlider/ImageSlider.js" type="text/javascript" ></script>
 
-<script type="text/javascript" >
-   ImageSlider.init(
-            {
-               container: "slider1",
-               timeout: 4
-            }   
-      );
-</script>
 </body>
 </html>
--- plugins/ImageSlider/ImageSlider.js
+++ plugins/ImageSlider/ImageSlider.js
@@ -42,6 +42,8 @@
                console.debug(textToDebug);
             }
     },
+    //theParametes: container->the object div where the images are showed. 
+    // timeout -> the time which image is showed.
     init:function (theParameters) {
        
        var methodName = "init";
@@ -53,8 +55,51 @@
        
        this.debug(methodName,"The container id where the images will be slided [ " + idContainer + " ] and the timeout [ " + timeout +" ] seconds");    
        
-       //get the properties, width and height of the container   
+       idContainer = '#'+idContainer;
        
+       var numImages = $(idContainer).find('img').length;
+       this.debug(methodName, "The container [ " + idContainer +" ] has [ " + numImages +" ] images");
+       var widthContainer =  $(idContainer).css("width");
+       var heightContainer =  $(idContainer).css("height");
+       this.debug(methodName, 'The container dimensions are: ' + widthContainer + ' x ' + heightContainer +'.');
+       
+       this.debug(methodName, "Set the max width to the images and hides them");       
+       
+       $(idContainer).find('img').each(
+          function (index) {
+              $( this ).css({"width":widthContainer,
+                             "height":heightContainer});
+              if (index > 0){
+                 $(this).hide();              
+              }
+          }       
+       );
+       
+       this.debug(methodName, "Create timer with timeout [ " + timeout + " ] seconds");
+       var idx = 1;
+       setInterval(function(){
+          ImageSlider.debug(methodName, "setInterval::timeout().Enter."+idContainer);
+          ImageSlider.debug(methodName, "setInterval::timeout().Enter."+idContainer+". Idx [ " + idx +" ]");
+          
+          $(idContainer).find('img').each(
+          function (index) {
+              
+              if (index != idx ){
+                 $(this).hide();              
+              }else{
+                 $(this).show();
+              }
+          }       
+       );
+          
+          if (idx < numImages-1){
+             idx++;
+          }else{
+             idx = 0;          
+          }
+          ImageSlider.debug(methodName, "setInterval::timeout().Exit."+idContainer);
+       }, 
+           timeout*1000);
        this.debugExit(methodName);
        
     }
