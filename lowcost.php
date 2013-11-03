a4d7f4bbb1100855584fd129eb296502b6dd347a
Esteban Sinobas Carpio <tebi@Ubuntu-Webserver.(none)>
2013-11-03 18:03:55 +0100

    Second Version

--- lowcost.php
+++ lowcost.php
@@ -1,35 +1,57 @@
 <div id="cuerpocenter_lowcost">
-	<object id="FlashID5" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="585" height="474">
-	  <param name="movie" value="img/lowcost.swf" />
-	  <param name="quality" value="high" />
-	  <param name="wmode" value="transparent" />
-	  <param name="swfversion" value="6.0.65.0" />
-	  <!-- Esta etiqueta param indica a los usuarios de Flash Player 6.0 r65 o posterior que descarguen la versión más reciente de Flash Player. Elimínela si no desea que los usuarios vean el mensaje. -->
-	  <param name="expressinstall" value="Scripts/expressInstall.swf" />
-	  <!-- La siguiente etiqueta object es para navegadores distintos de IE. Ocúltela a IE mediante IECC. -->
-	  <!--[if !IE]>-->
-	  <object type="application/x-shockwave-flash" data="img/lowcost.swf" width="585" height="474">
-	    <!--<![endif]-->
-	    <param name="quality" value="high" />
-	    <param name="wmode" value="transparent" />
-	    <param name="swfversion" value="6.0.65.0" />
-	    <param name="expressinstall" value="Scripts/expressInstall.swf" />
-	    <!-- El navegador muestra el siguiente contenido alternativo para usuarios con Flash Player 6.0 o versiones anteriores. -->
-	    <div>
-	      <h4>El contenido de esta p&aacute;gina requiere una versi&oacute;n m&aacute;s reciente de Adobe Flash Player.</h4>
-	      <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Obtener Adobe Flash Player" width="112" height="33" /></a></p>
-        </div>
-	    <!--[if !IE]>-->
-      </object>
-	  <!--<![endif]-->
-  </object>
-<div id="texto_lowcost">
-   	  <span class="titulo">CONCEPTO <b>LOW COST</b></span>
+   <script src="./plugins/ImageSlider/ImageSlider.js" type="text/javascript" ></script>
+   <?php
+     include_once('./php/DirectoryFunctions.php'); 
+   ?>
+	<div id="cuerpocenter_lowcost_cuerpo1">
+	<?php
+      $files = getDirectoryFiles('./img/lowcost/lowcost1');
+      for ($i = 0; $i < count($files);$i++){
+         printf("<img src=\"%s\" alt=\"\">\n", $files[$i]);
+      }
+   ?>
+	
+	</div>
+	
+	<script type="text/javascript" >
+      ImageSlider.init(
+      {
+         container: "cuerpocenter_lowcost_cuerpo1",
+         timeout: 3
+        
+      }   
+      );
+  </script>
+	
+	
+   
+   <div id="texto_lowcost">
+   <span class="titulo">CONCEPTO <b>LOW COST</b></span>
       <p class="tp">
         No tienes excusa para no estar en forma y pasarlo fenomenal. Todo, pero todo el gimnasio destinado para ti, por sólo 25&euro;/mes. <b>Y ese es el precio final. </b>
         </p>
         <p class="tp">
         Así es la filosofía low cost que hemos querido ofrecerte para que todo Leganés disfrute de un mundo de ejercicios, máquinas de última generación, clases colectivas y asesores de fitness, sólo para ti. <b>Sólo por 25&euro;/mes.</b>
         </p>
+   	
     </div>
+    
+    <div id="cuerpocenter_lowcost_cuerpo2">
+       <?php
+      $files = getDirectoryFiles('./img/lowcost/lowcost2');
+      for ($i = 0; $i < count($files);$i++){
+         printf("<img src=\"%s\" alt=\"\">\n", $files[$i]);
+      }
+      ?>
+
+	</div>
+	<script type="text/javascript" >
+      ImageSlider.init(
+      {
+         container: "cuerpocenter_lowcost_cuerpo2",
+         timeout: 2
+        
+      }   
+      );
+  </script>
 </div>
