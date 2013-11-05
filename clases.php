f739a3ef1dac3ddf95ad3369e5332d420cea0938
Esteban Sinobas Carpio <esteban.sinobas@gmail.com>
2013-11-05 09:28:13 +0100

    Clases Colectivas

--- clases.php
+++ clases.php
@@ -1,33 +1,14 @@
+<script src="./plugins/ImageSlider/ImageSlider.js" type="text/javascript" ></script>
+   <?php
+     include_once('./php/DirectoryFunctions.php'); 
+   ?>
 <div id="cuerpocenter_clases_borde_top"></div>
 <div id="cuerpocenter_clases">
 	
-	<object id="FlashID2" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="585" height="474">
-	  <param name="movie" value="img/colectiva.swf" />
-	  <param name="quality" value="high" />
-	  <param name="wmode" value="transparent" />
-	  <param name="swfversion" value="6.0.65.0" />
-	  <!-- Esta etiqueta param indica a los usuarios de Flash Player 6.0 r65 o posterior que descarguen la versión más reciente de Flash Player. Elimínela si no desea que los usuarios vean el mensaje. -->
-	  <param name="expressinstall" value="Scripts/expressInstall.swf" />
-	  <!-- La siguiente etiqueta object es para navegadores distintos de IE. Ocúltela a IE mediante IECC. -->
-	  <!--[if !IE]>-->
-	  <object type="application/x-shockwave-flash" data="img/colectiva.swf" width="585" height="474">
-	    <!--<![endif]-->
-	    <param name="quality" value="high" />
-	    <param name="wmode" value="transparent" />
-	    <param name="swfversion" value="6.0.65.0" />
-	    <param name="expressinstall" value="Scripts/expressInstall.swf" />
-	    <!-- El navegador muestra el siguiente contenido alternativo para usuarios con Flash Player 6.0 o versiones anteriores. -->
-	    <div>
-	      <h4>El contenido de esta p&aacute;gina requiere una versi&oacute;n m&aacute;s reciente de Adobe Flash Player.</h4>
-	      <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Obtener Adobe Flash Player" width="112" height="33" /></a></p>
-        </div>
-	    <!--[if !IE]>-->
-      </object>
-	  <!--<![endif]-->
-  </object>
+	
 <div id="texto_clases">
     <span class="titulo">CLASES COLECTIVAS</span>
-    <div style="width:340px">
+    <div style="width:340px; float:left;background-color:white">
     	<p>
 			<b>Aerobic-Step:</b> ¡No pierdas el paso! Y únete a la clase más divertida de Leganés. Los pasos más dinámicos para moldear tu cuerpo y quemar calorías te están esperando.
         </p>
@@ -47,8 +28,27 @@
         	<b>Tai-Chi</b> La tradición oriental para mejorar y restablecer tu salud. Deja llevarte por la filosofía tradicional china controlando la respiración y equilibrando tu cuerpo.
         </p>
      </div>
+     </div>
+     
+     <div id="clases_slider_image">
+        <?php
+           $files = getDirectoryFiles('./img/clases');
+           for ($i = 0; $i < count($files);$i++){
+             printf("<img src=\"%s\" alt=\"\">\n", $files[$i]);
+           }
+        ?>
+        <script type="text/javascript" >
+           ImageSlider.init(
+           {
+              container: "clases_slider_image",
+              timeout: 2.5
+        
+           }   
+           );
+        </script>
+     </div>
      <br />
-        <div style="background-color:#DFDFDF;padding:10px">
+        <div id="reglamento_uso">
         <b>Reglamento de uso:</b>
         
         <ol>
@@ -87,7 +87,7 @@
         
         </ol>
         </div>
-    </div>
+    
 </div>
 <div id="cuerpocenter_clases_borde_bottom"></div>
 <div style="text-align:center"><img src="img/tu_fitness.jpg" /></div>
